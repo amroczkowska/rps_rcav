030556d114e27037cf572c58e1e31546dc4b3772
Aleksandra Mroczkowska <am679@cornell.edu>
2016-10-21 20:45:56 -0500

    hello

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ get("/", {:controller=> "game", :action=>"user_plays_rock"  })
 get("/paper", {:controller=> "game", :action=>"user_plays_paper"  })
 get("/scissors", {:controller=> "game", :action=>"user_plays_scissors"  })
 
+   
 
 
 
